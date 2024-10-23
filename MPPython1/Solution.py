fbdf1269c3b552956a8d9bcfde282794c19e41c2
Roary <rrichglitch@gmail.com>
2024-10-23 15:36:48 -0400

    P1 is done!

--- MPPython1/Solution.py
+++ MPPython1/Solution.py
@@ -12,12 +12,43 @@ class Solution:
         self.graph = graph
         self.info = info
 
+    def local_bfs_path(self, graph, isp, list_clients):
+
+        paths = {}
+
+        graph_size = len(graph)
+        priors = [-1]*graph_size
+        search_queue = deque()
+        search_queue.append(isp)
+
+        while search_queue:
+            node = search_queue.popleft()
+            for neighbor in graph[node]:
+                if (priors[neighbor] == -1 and neighbor != isp):
+                    priors[neighbor] = node
+                    search_queue.append(neighbor)
+
+        for client in list_clients:
+            path = []
+            current_node = client
+            while (current_node != -1):
+                path.append(current_node)
+                current_node = priors[current_node]
+            path = path[::-1]
+            paths[client] = path
+
+        return paths
+
+
     def output_paths(self):
         """
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
         """
         paths, bandwidths, priorities = {}, {}, {}
+
+        paths = self.local_bfs_path(self.graph, self.isp, self.info["list_clients"])
+
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
         return (paths, bandwidths, priorities)
