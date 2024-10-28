913921651956a23689b82dedf56fa0acc38f07e1
Roary <rrichglitch@gmail.com>
2024-10-28 10:47:11 -0400

    started adding the score func

--- MPPython2/Solution.py
+++ MPPython2/Solution.py
@@ -11,24 +11,44 @@ class Solution:
         self.isp = isp
         self.graph = graph
         self.info = info
+        self.scores = [-1]*len(self.graph)
+        self.connecteced_component = None
 
-    def score(self,node):
-        return (0,node)
+    def will_pay(self,node):
+        return 1
+
+    def score(self, node, path_bandwidth):
+        if scores[node] >= 0:
+            return scores[node]
+
+        score = node.pmt * self.will_pay(node)
+
+        child_scores = [ self.score(child,path_bandwidth-1,)*(1-self.connecteced_component[child]) for child in self.graph[node]]
+
+        child_score.sort(reverse=True)
+
+        for i in range(node.bandwidth):
+            score += child_scores[i]
+            
+        scores[node] = score
+        return score
 
     def output_paths(self):
         """
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
         """
-        connecteced_component = set([self.isp])
+        self.connecteced_component = [0]*len(self.graph)
 
         possibleNext = set(self.graph[self.isp])
         
         print(f"len of pos next is {len(self.graph)}")
 
+        path_bandwidth = 0      # TODO
+
         while(len(possibleNext)):
 
-            childScores = sorted(possibleNext, reverse= True, key=lambda x: self.score(x))
+            childScores = sorted(possibleNext, reverse= True, key=lambda x: self.score(x,path_bandwidth,connecteced_component))
 
             connecteced_component.add(childScores[0])
 
@@ -36,10 +56,7 @@ class Solution:
 
             possibleNext.update(self.graph[childScores[0]])
 
-            possibleNext.difference_update(connecteced_component)  #this sucks, we can do better but we lazy rn
-
-
-        
+            possibleNext.difference_update(connecteced_component)  #this sucks, we can do better but we lazy rn        
 
 
         paths, bandwidths, priorities = {}, {}, {}
