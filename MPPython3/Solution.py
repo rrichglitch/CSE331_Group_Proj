f98acf7cc5658810ad42d62ef98c2d543828e1ec
Roary <rrichglitch@gmail.com>
2024-11-26 01:11:53 -0500

    Cleaned up and polished code for problem 3. Did some work in P2 as well just for testing.

--- MPPython3/Solution.py
+++ MPPython3/Solution.py
@@ -1,5 +1,6 @@
-import heapq
+from heapq import heapify,heappush,heappop
 import sys
+from math import ceil
 
 class Solution:
 
@@ -9,7 +10,8 @@ class Solution:
         self.graph = graph
         self.info = info
         self.node2downStream = [set() for _ in range(len(self.graph))]
-        self.current_path_acceptable = [-1] * len(self.graph)
+        self.m_extra_path_delay = [-1] * len(self.graph)
+        self.m_upgrade_for_path_delay_improvement = [(-1,-1)] * len(self.graph)
         self.current_path_compliant = [-1] * len(self.graph)  # For complaint threshold
 
         # Ensure 'cost_bandwidth' is present
@@ -28,8 +30,29 @@ class Solution:
         # TUNE THIS MAGIC NUMBER [0,1]:
         self.top_x_percent = 0.8
 
-    def extra_path_delay(self, node):  # O(1)
+
+    def extra_node_delay(self, node): # O(1)
         return len(self.node2downStream[node]) // self.info["bandwidths"][node]
+
+    def extra_path_delay_r(self, path, i): # O(1) per new checked node
+        if i < 0: return 0
+
+        end_node = path[i]
+
+        if self.m_extra_path_delay[end_node] > 0: return self.m_extra_path_delay[end_node]
+
+        self.m_extra_path_delay[end_node] = max( self.extra_node_delay(end_node), self.extra_path_delay_r(path, i-1) )
+
+        return self.m_extra_path_delay[end_node]
+
+    def extra_path_delay(self, path, refresh): # O(1) per new checked node
+        if len(path) <= 1: return 0
+
+        if refresh: # refresh once per call then allow the recursive tree to cache again
+            for i in range(len(path)):
+                self.m_extra_path_delay[path[i]] = -1
+
+        return self.extra_path_delay_r(path, i=len(path)-2) # i is index and should exclude this end node
 
     # node2downStream must be populated before calling this method
     def dijkstras(self, grumps):  # grumps are the nodes to return paths for
@@ -41,14 +64,14 @@ class Solution:
         visited = [False]*len(self.graph) # unfortunately need this bc distances are updated before visit
         
         while len(pq):
-            cur_dist, cur_node = heapq.heappop(pq)
+            cur_dist, cur_node = heappop(pq)
             
             # If we've already processed this vertex, skip it
             if visited[cur_node]: continue  
             else: visited[cur_node] = True
             
             for child in self.graph[cur_node]:
-                weight = 1+self.extra_path_delay(child)
+                weight = 1+self.extra_node_delay(child)
 
                 distance = cur_dist + weight
                 
@@ -56,7 +79,7 @@ class Solution:
                 if distance < distances[child]:
                     distances[child] = distance
                     path_src[child] = cur_node
-                    heapq.heappush(pq, (distance, child))
+                    heappush(pq, (distance, child))
 
         # extract paths
         grump_paths = {}
@@ -70,72 +93,82 @@ class Solution:
 
         return grump_paths
 
-    # This gives a heuristic score based on pmt, path_len, delay, acceptability, and complaint threshold O(n)
-    def score_current_path(self, path):  # call only on client paths
+
+    # call only on client paths      O(n)
+    def path_is_acceptable(self,path,refresh):
         node = path[len(path)-1]
 
         base_delay = len(self.short_paths[node])-1
-        extra_delay = max([ self.extra_path_delay(path[i]) for i in range(len(path)-1) ])
-        cur_delay = base_delay+extra_delay
+        extra_delay = self.extra_path_delay(path,refresh)
+        cur_delay = base_delay + extra_delay
 
-        self.current_path_acceptable[node] = cur_delay <= (base_delay * self.info["alphas"][node])
-        self.current_path_compliant[node] = cur_delay <= (base_delay * self.info["betas"][node])
+        return cur_delay <= (base_delay * self.info["alphas"][node])
 
-        money = self.info["payments"][node] * self.current_path_acceptable[node]
-
-        # Penalize if complaint threshold is not met
-        penalty = 0
-        if not self.current_path_compliant[node]:
-            
-            penalty = money  
-
-        return (money - penalty) / cur_delay if cur_delay != 0 else 0
-
-    # Check if the path is acceptable based on alpha
-    def check_path_acceptable(self, path):
-        node = path[-1]
-
-        if self.current_path_acceptable[node] >= 0:
-            return self.current_path_acceptable[node]
-
-        base_delay = len(self.short_paths[node]) - 1
-        extra_delay = max([self.extra_path_delay(path[i]) for i in range(len(path) - 1)])
-
-        return (base_delay + extra_delay) <= (base_delay * self.info["alphas"][node])
-
-    # New method to check complaint threshold based on beta
-    def check_complaint_threshold(self, path):
+    # This gives a heuristic score based on pmt, path_len, delay, and acceptability   O(n)
+    def score_path(self,path): # call on new client paths
         node = path[len(path)-1]
 
-        if self.current_path_compliant[node] >= 0:
-            return self.current_path_compliant[node] # any new paths will already be cached during scoring
+        money = self.info["payments"][node] * self.path_is_acceptable(path,refresh=True)
 
         base_delay = len(self.short_paths[node])-1
-        extra_delay = max([ self.extra_path_delay(path[i]) for i in range(len(path)-1) ])
+        extra_delay = self.extra_path_delay(path,refresh=False)
+        cur_delay = base_delay + extra_delay
 
-        return (base_delay+extra_delay) <= (base_delay * self.info["betas"][node])
+        return money/cur_delay
 
-    
+    # returns the delay that would have to be reduced for this node to stop complaining
+    def path_complaint_diff(self, path,refresh=False):
+        node = path[len(path)-1]
 
-    # Update delays and acceptability for given clients
-    def update_delays(self, clients):
-        for c in clients:
-            path = self.paths[c]
-            base_delay = len(self.short_paths[c]) - 1
-            extra_delay = max([self.extra_path_delay(path[i]) for i in range(len(path) - 1)])
-            cur_delay = base_delay + extra_delay
+        base_delay = len(self.short_paths[node])-1
+        extra_delay = self.extra_path_delay(path,refresh=refresh)
+        cur_delay = base_delay + extra_delay
 
-            
-            self.current_path_acceptable[c] = cur_delay <= (base_delay * self.info["alphas"][c])
+        return cur_delay - (base_delay * self.info["alphas"][node])
 
-            
-            self.current_path_compliant[c] = cur_delay <= (base_delay * self.info["betas"][c])
+
+    def add_band_for_complaints(self, fine_amount, thresh_beta):
+
+        complaint_thresh = int(thresh_beta * len(self.S)) # this might be under by 1??
+
+        complainers = [ c for c in self.C if self.current_path_compliant[c] and c in self.S ]
+
+        band_budget = fine_amount/self.cost_bandwidth  # Maximum amount we are willing to spend in terms of bandwidth
+
+        no_upgrades = self.info["bandwidths"].copy()
+
+        # sort the complainers by how easy it is to keep them from complaining "complaint_diff"
+        complainers = sorted( ( (self.path_complaint_diff(self.paths[node]),node) for node in complainers ), reverse=True)
+        while len(complainers) > complaint_thresh and band_budget > 0 :
+
+            _, node_to_appease = complainers.pop()
+
+            delay_thresh = (len(self.short_paths[node_to_appease])-1) * (self.info["betas"][node_to_appease]-1)
+            delay_thresh = ceil(delay_thresh)
+
+            path = self.paths[node_to_appease]
+            for i in range(len(path)-2):
+                upgrade_node = path[i]
+                extra_delay = self.extra_node_delay(upgrade_node)
+                delay_to_drop = extra_delay - delay_thresh
+
+                if delay_to_drop > 0:
+                    # drop the delay all at once with multiplication
+                    amnt_to_upgrade = len(self.node2downStream[upgrade_node]) % self.info["bandwidths"][upgrade_node]
+                    amnt_to_upgrade += len(self.node2downStream[upgrade_node]) * (delay_to_drop-bool(amnt_to_upgrade))
+                    self.info["bandwidths"][upgrade_node] += amnt_to_upgrade
+                    band_budget -= amnt_to_upgrade
+
+        # if we were unable to upgrade enough to avoid fine then dont upgrade at all
+        if len(complainers) > complaint_thresh:
+            self.info["bandwidths"] = no_upgrades
+
 
     # to begin we run our MST algo
     # next we iterate through the paths created by the MST and check for bandwidth delays
     # we sort our nodes which are unhappy with bandwidth delay by pmt and/or path len
     # find a new short path for unhappy nodes given the current graph (dijkstra) 
-    def improve_MST(self):
+    def iterative_improved_paths(self):
         paths = {}
 
         first = True
@@ -154,7 +187,7 @@ class Solution:
                 first = False
 
             # add in top x% of paths sorted by payment or sorted by path_len or both
-            sorted_new_paths = sorted(new_paths.values(), reverse=True, key=self.score_current_path) # O(n^2)
+            sorted_new_paths = sorted(new_paths.values(), reverse=True, key=self.score_path) # O(n^2)
             new_cnt = len(new_paths)
             for i in range(min(new_cnt,int(new_cnt*self.top_x_percent)+1)): # tune this magic top_x_percent!
                 path = sorted_new_paths[i]
@@ -166,97 +199,28 @@ class Solution:
             unhappy = []
             for c in self.info["list_clients"]:
                 if c not in paths: unhappy.append(c)
-                else:
-                    if c not in new_paths: self.current_path_acceptable[c] = -1 # mark old paths for rechecking delay
-                    if not self.check_path_acceptable(paths[c]): # O(n^2)
-                        unhappy.append(c)
-                        # as we find unhappy clients, pull them from their current path
-                        for upstream in paths[c]: # O(n^2)
-                            self.node2downStream[upstream].discard(c)
+                elif not self.path_is_acceptable(paths[c],refresh=(c not in new_paths)): # O(n^2)
+                    unhappy.append(c)
+                    # as we find unhappy clients, pull them from their current path
+                    for upstream in paths[c]: # O(n^2)
+                        self.node2downStream[upstream].discard(c)
 
         # print(paths)
         for c in self.info["list_clients"]:
             paths.setdefault(c,[self.isp,c]) # add in default paths for the shitters that didnt make the cut
 
-        self.paths = paths  # Store the final paths
+        return paths  # output the final paths
 
-        
-        total_bandwidth_cost = 0
-
-        
-        self.update_delays(self.C)
-        complaining_clients = [c for c in self.C if not self.current_path_compliant[c]]
-
-        # FCC penalty
-        complaining_S = [c for c in complaining_clients if c in self.S]
-        threshold_fcc = int(self.rho_fcc * len(self.S))
-        if len(complaining_S) > threshold_fcc and self.fcc_fine > 0:
-            # reduce number of complaining clients in S
-            penalty_amount = self.fcc_fine
-            budget = self.fcc_fine  # Maximum amount weare willing to spend
-            total_spent = 0
-            while len(complaining_S) > threshold_fcc and total_spent < budget:
-                # try to fix thier delay
-                for c in complaining_S:
-                    path = self.paths[c]
-                    # find nodes along path to increase bandwidth
-                    nodes_to_consider = path[:-1]  # Exclude client node
-                    nodes_to_consider = sorted(nodes_to_consider, key=lambda u: self.extra_path_delay(u), reverse=True)
-                    for u in nodes_to_consider:
-                        
-                        self.info["bandwidths"][u] += 1
-                        total_bandwidth_cost += self.cost_bandwidth
-                        total_spent += self.cost_bandwidth
-                        # Update delays for clients impacted by node u
-                        impacted_clients = list(self.node2downStream[u])
-                        self.update_delays(impacted_clients)
-                        # calculating again complaining clients in S
-                        complaining_S = [c for c in self.S if not self.current_path_compliant[c]]
-                        if len(complaining_S) <= threshold_fcc or total_spent >= budget:
-                            break
-                    if len(complaining_S) <= threshold_fcc or total_spent >= budget:
-                        break
-
-        # lawsuit penalty
-        complaining_C = [c for c in self.C if not self.current_path_compliant[c]]
-        threshold_lawsuit = int(self.rho_lawsuit * len(self.C))
-        if len(complaining_C) > threshold_lawsuit and self.lawsuit_amount > 0:
-            # reduce number of complaining clients in C
-            penalty_amount = self.lawsuit_amount
-            budget = self.lawsuit_amount  # Maximum we are willing to spend
-            total_spent = 0
-            while len(complaining_C) > threshold_lawsuit and total_spent < budget:
-                # try to fix their delay
-                for c in complaining_C:
-                    path = self.paths[c]
-                    
-                    nodes_to_consider = path[:-1]  
-                    nodes_to_consider = sorted(nodes_to_consider, key=lambda u: self.extra_path_delay(u), reverse=True)
-                    for u in nodes_to_consider:
-                        # decide whether to increase bandwidth at node u
-                        self.info["bandwidths"][u] += 1
-                        total_bandwidth_cost += self.cost_bandwidth
-                        total_spent += self.cost_bandwidth
-                        # Update delays 
-                        impacted_clients = list(self.node2downStream[u])
-                        self.update_delays(impacted_clients)
-                        # calcautking again complaining clients in C
-                        complaining_C = [c for c in self.C if not self.current_path_compliant[c]]
-                        if len(complaining_C) <= threshold_lawsuit or total_spent >= budget:
-                            break
-                    if len(complaining_C) <= threshold_lawsuit or total_spent >= budget:
-                        break
-
-        return self.paths
 
     def output_paths(self):
         """
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
         """
-        paths = self.improve_MST()
+        self.paths = self.iterative_improved_paths()
+
+        self.add_band_for_complaints(self.fcc_fine, self.rho_fcc)
+        self.add_band_for_complaints(self.lawsuit_amount, self.rho_lawsuit)
+
         # For problems 3, 4, 5, return updated bandwidths
-        updated_bandwidths = self.info["bandwidths"]
-        # For problems 4 and 5, return priorities if needed. Assuming this is Problem 3, return empty dict
-        priorities = {}
-        return paths, updated_bandwidths, priorities
+        return self.paths, self.info["bandwidths"], {}
